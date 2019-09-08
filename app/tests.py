77e4760e788a07847a6c94d9cdc0750fbaa08c0c
James Kirwa <example@email.com>
2019-09-08 10:03:07 +0300

    test the project

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import profile,project,rating
+import datetime as dt
+# Create your tests here.
+
+class EditorTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
 
 # Create your tests here.
