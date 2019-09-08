08c2351b69719ed811a71348177b1c429522983b
James Kirwa <example@email.com>
2019-09-08 09:39:47 +0300

    import django inmanage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     try:
-        if len(full_path) == 1:
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
@@ -18,6 +17,7 @@ if __name__ == "__main__":
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
+                "django succefully imported"
             )
         raise
     execute_from_command_line(sys.argv)
